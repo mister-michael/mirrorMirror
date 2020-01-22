58587565060760f1ab07cf54bdaa4630557903d3
Michael Clark <michaelclarknashville@protonmail.com>
2020-01-22 13:47:19 -0600

    working product, textappears in mirror boxes with strange spacing, text disappears from entry field when typed

---
+++ main.js
@@ -0,0 +1,20 @@
+// const boxBox
+const boxOne = document.querySelector("#boxOne");
+const boxTwo = document.querySelector("#boxTwo");
+
+const captureText = () => {
+ const entryField = document.getElementById("message")
+
+ boxOne.innerHTML += `
+ ${entryField.value}
+ `;
+
+ boxTwo.innerHTML += `
+ ${entryField.value}
+ `;
+
+ entryField.value= ""
+ 
+}
+
+document.getElementById("message").addEventListener("keyup", captureText);
